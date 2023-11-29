85e7738446a6059490bf402cc27d575239480404
John Keno Iweh <john.iweh@innocentmichael.com>
2023-11-29 23:28:22 +0100

    hot fix

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,7 +3,12 @@ import { RouterModule, Routes } from '@angular/router';
 import { SignupLoginComponent } from './pages/signup-login/signup-login.component';
 import { LandingComponent } from './pages/landing/landing.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: LandingComponent
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
